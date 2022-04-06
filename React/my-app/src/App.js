01e644d6d1e138f2ecbbe4cf0378d08f1a519c5a
nick-chaika <58843860+nick-chaika@users.noreply.github.com>
2022-04-06 21:31:23 +0100

    react

---
+++ React/my-app/src/App.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+      <div className="app-wrapper">
+        <header className='header'>
+          <img src="https://www.google.com/imgres?imgurl=https%3A%2F%2Fi.pinimg.com%2F736x%2Fde%2Fa0%2Ff3%2Fdea0f3b7f480b1151c08db4a402a43b9.jpg&imgrefurl=https%3A%2F%2Fwww.pinterest.com%2Fpin%2F140526450859583811%2F&tbnid=hW5ft5-M26mf_M&vet=12ahUKEwjP2qO57v_2AhWn8LsIHTh2AncQMygnegUIARCPAg..i&docid=4JCEDR7i-cWCuM&w=736&h=736&q=logo&ved=2ahUKEwjP2qO57v_2AhWn8LsIHTh2AncQMygnegUIARCPAg" alt=""/>
+        </header>
+        <nav className='nav'>
+          <div>Profile</div>
+          <div>Messages</div>
+          <div>News</div>
+          <div>Music</div>
+          <div>Settings</div>
+        </nav>
+        <div className='content'>
+          <div>
+            <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fbipbap.ru%2Fkrasivye-kartinki%2Fkartinki-krasivye-more-36-foto.html&psig=AOvVaw3ivYpYoSdrml5eQGHw4T4q&ust=1649360711421000&source=images&cd=vfe&ved=0CAoQjRxqFwoTCKD8jc2ZgPcCFQAAAAAdAAAAABAJ" alt=""/>
+          </div>
+          <div>
+            Ava dis
+          </div>
+          <div>
+            My post
+            <div>
+              New post
+            </div>
+            <div>
+              <div>
+                post 1
+              </div>
+              <div>
+                post 2
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+  );
+}
+
+export default App;
