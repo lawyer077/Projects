d236942d207079c7a639ace8dc404880bee2eb6f
nick-chaika <58843860+nick-chaika@users.noreply.github.com>
2022-04-07 21:46:54 +0100

    react

---
+++ React/my-app/src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Header = () => {
+    return <header className='header'>
+                <img src="https://www.google.com/imgres?imgurl=https%3A%2F%2Fi.pinimg.com%2F736x%2Fde%2Fa0%2Ff3%2Fdea0f3b7f480b1151c08db4a402a43b9.jpg&imgrefurl=https%3A%2F%2Fwww.pinterest.com%2Fpin%2F140526450859583811%2F&tbnid=hW5ft5-M26mf_M&vet=12ahUKEwjP2qO57v_2AhWn8LsIHTh2AncQMygnegUIARCPAg..i&docid=4JCEDR7i-cWCuM&w=736&h=736&q=logo&ved=2ahUKEwjP2qO57v_2AhWn8LsIHTh2AncQMygnegUIARCPAg" alt=""/>
+            </header>
+}
+
+export default Header;
+
